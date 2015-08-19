b9fb310736a3026642fd44f8e870b94d997fbf17
Manuel Vila <mvila@3base.com>
2015-08-19 11:11:09 +0800

    update react-bootstrap to 0.23.7

--- src/dialog.js
+++ src/dialog.js
@@ -6,62 +6,66 @@ let BS = require('react-bootstrap');
 let Dialog = {
   inject(ui) {
     return React.createClass({
-      mixins: [ui.DialogCommon, BS.OverlayMixin],
+      mixins: [ui.DialogCommon],
 
       getDialogDOMNode() {
-        return this.getOverlayDOMNode();
+        return this.refs.portal.getOverlayDOMNode();
       },
 
       render() {
-        return React.DOM.span(null);
+        return (
+          <BS.Portal ref='portal' container={this}>
+            {this.renderOverlay()}
+          </BS.Portal>
+        );
       },
 
       renderOverlay() {
-        if (!this.state.isOpen) return React.DOM.span(null);
+        if (!this.state.isOpen) return false;
         return (
-          React.createElement(BS.Modal,
-            {
-              title: this.state.options.title,
-              animation: this.state.options.animation,
-              onRequestHide: this.onHide
-            },
-            React.DOM.div(
-              { className: 'modal-body' },
-              this.state.options.message ?
-                React.DOM.div({ style: { whiteSpace: 'pre-line' } }, this.state.options.message) :
-                false,
-              this.state.options.content ? this.state.options.content : false,
-              this.state.options.input ?
-                React.DOM.div(
-                  { className: 'form-group', style: { marginBottom: '2px' } },
-                  React.DOM.label({ htmlFor: 'input' }, this.state.options.input.label),
-                  React.DOM.input({
-                    type: 'text',
-                    className: 'form-control',
-                    value: this.state.inputValue,
-                    onChange: this.onChange,
-                    onKeyPress: this.onKeyPress,
-                    id: 'input'
-                  })
-                ) :
+          <BS.Modal animation={this.state.options.animation} onHide={this.onHide}>
+            <BS.Modal.Header closeButton>
+              <BS.Modal.Title>{this.state.options.title}</BS.Modal.Title>
+            </BS.Modal.Header>
+            <BS.Modal.Body>
+              {
+                this.state.options.message ?
+                <div style={{ whiteSpace: 'pre-line' }}>
+                  {this.state.options.message}
+                </div> :
                 false
-            ),
-            React.DOM.div(
-              { className: 'modal-footer' },
-              React.createElement(BS.ButtonToolbar, null,
-                this.state.options.secondaryButton ?
-                  React.createElement(BS.Button,
-                    { onClick: this.onSecondaryButton },
-                    this.state.options.secondaryButton.label
-                  ) :
-                  false,
-                React.createElement(BS.Button,
-                  { bsStyle: 'primary', onClick: this.onPrimaryButton },
-                  this.state.options.primaryButton.label
-                )
-              )
-            )
-          )
+              }
+              {
+                this.state.options.content ?
+                this.state.options.content :
+                false
+              }
+              {
+                this.state.options.input ?
+                <div className='form-group' style={{ marginBottom: '2px' }}>
+                  <label htmlFor='input'>
+                    {this.state.options.input.label}
+                  </label>
+                  <input type='text' className='form-control' value={this.state.inputValue} onChange={this.onChange} onKeyPress={this.onKeyPress} id='input' />
+                </div> :
+                false
+              }
+            </BS.Modal.Body>
+            <BS.Modal.Footer>
+              <BS.ButtonToolbar>
+                {
+                  this.state.options.secondaryButton ?
+                  <BS.Button onClick={this.onSecondaryButton}>
+                    {this.state.options.secondaryButton.label}
+                  </BS.Button> :
+                  false
+                }
+                <BS.Button bsStyle='primary' onClick={this.onPrimaryButton}>
+                  {this.state.options.primaryButton.label}
+                </BS.Button>
+              </BS.ButtonToolbar>
+            </BS.Modal.Footer>
+          </BS.Modal>
         );
       }
     });
